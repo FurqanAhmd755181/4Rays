48b924f1cf8fb7182cf0d9726ab00a4db9fc93bd
Nabil <cleo.master0000@gmail.com>
2024-10-26 22:54:20 +0500

    Fixed SignUp and SignIn Routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,12 +1,22 @@
 import {createBrowserRouter} from "react-router-dom";
 import UserLayout from "../pages/User/Layout/UserLayout.jsx";
 import UserRoutes from "./UserRoutes.jsx";
+import Signinpage from "../pages/authpages/Signinpage/Signinpage.jsx";
+import SignUpPage from "../pages/authpages/Signuppage/SignUpPage.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <UserLayout />,
         children: UserRoutes
+    },
+    {
+        path: "/sign-in",
+        element: <Signinpage />
+    },
+    {
+        path: "/sign-up",
+        element: <SignUpPage />
     }
 ])
 
