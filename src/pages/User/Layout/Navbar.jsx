a4eebee9845096d5abc2014b92fa5bce4edd32b3
Huzaifa Sajjad <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-10-26 10:32:12 -0700

    add signup and signin pages

--- src/pages/User/Layout/Navbar.jsx
+++ src/pages/User/Layout/Navbar.jsx
@@ -23,7 +23,6 @@ const Navbar = () => {
 
   return (
     <div className="px-44">
-<<<<<<< Updated upstream
       <div className="flex items-center justify-center gap-64 border-t-2 border-b-2 border-[#C20009] py-5">
         <nav className="flex items-center gap-20">
           <Link>Features</Link>
@@ -31,31 +30,6 @@ const Navbar = () => {
           <Link to="/pricing">Prices</Link>
           <Link to="/about">About</Link>
         </nav>
-=======
-  <div className="px-44 flex items-center justify-center gap-64 border-t-2 border-b-2 border-[#D9D9D9] py-5">
-    <nav className="flex items-center gap-20">
-      <Link className="hover:text-[#BC1E2D] transition-colors">Features</Link>
-      <Link className="hover:text-[#BC1E2D] transition-colors">Resources</Link>
-      <Link to="/pricing" className="hover:text-[#BC1E2D] transition-colors">Prices</Link>
-      <Link to="/about" className="hover:text-[#BC1E2D] transition-colors">About</Link>
-    </nav>
-    <div className="flex items-center gap-3">
-      <div className="flex items-center gap-2">
-        <p className="text-[15px] font-[500]">Eng</p>
-        <span>
-          <FaChevronDown />
-        </span>
-      </div>
-      <span>
-        <FiShoppingCart size={20} color="#BC1E2D" />
-      </span>
-      <span>
-        <LuUser2 size={20} color="#BC1E2D" />
-      </span>
-    </div>
-  </div>
-</div>
->>>>>>> Stashed changes
 
         <div className="flex items-center gap-3 relative">
           <div className="flex items-center gap-2 cursor-pointer" onClick={toggleLanguageDropdown}>
