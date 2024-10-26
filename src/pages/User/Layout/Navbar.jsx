02f72646fc72722cde0f728f11d1dfda9f22b019
Huzaifa Sajjad <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-10-26 09:13:45 -0700

    add hover effect

--- src/pages/User/Layout/Navbar.jsx
+++ src/pages/User/Layout/Navbar.jsx
@@ -6,29 +6,30 @@ import { LuUser2 } from "react-icons/lu";
 const Navbar = () => {
   return (
     <div className="px-44">
-      <div className=" px-44 flex items-center justify-center gap-64 border-t-2 border-b-2 border-[#D9D9D9] py-5">
-        <nav className="flex items-center gap-20">
-          <Link>Features</Link>
-          <Link>Resources</Link>
-          <Link to="/pricing">Prices</Link>
-          <Link>About</Link>
-        </nav>
-        <div className="flex items-center gap-3">
-          <div className="flex items-center gap-2">
-            <p className="text-[15px] font-[500]">Eng</p>
-            <span>
-              <FaChevronDown />
-            </span>
-          </div>
-          <span>
-            <FiShoppingCart size={20} color="#BC1E2D" />
-          </span>
-          <span>
-            <LuUser2 size={20} color="#BC1E2D" />
-          </span>
-        </div>
+  <div className="px-44 flex items-center justify-center gap-64 border-t-2 border-b-2 border-[#D9D9D9] py-5">
+    <nav className="flex items-center gap-20">
+      <Link className="hover:text-[#BC1E2D] transition-colors">Features</Link>
+      <Link className="hover:text-[#BC1E2D] transition-colors">Resources</Link>
+      <Link to="/pricing" className="hover:text-[#BC1E2D] transition-colors">Prices</Link>
+      <Link className="hover:text-[#BC1E2D] transition-colors">About</Link>
+    </nav>
+    <div className="flex items-center gap-3">
+      <div className="flex items-center gap-2">
+        <p className="text-[15px] font-[500]">Eng</p>
+        <span>
+          <FaChevronDown />
+        </span>
       </div>
+      <span>
+        <FiShoppingCart size={20} color="#BC1E2D" />
+      </span>
+      <span>
+        <LuUser2 size={20} color="#BC1E2D" />
+      </span>
     </div>
+  </div>
+</div>
+
   );
 };
 
