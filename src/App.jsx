75a0372b070dee6089c8bc49533603f49d480ff5
Nabil <cleo.master0000@gmail.com>
2024-10-26 15:15:20 +0500

    A change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+
+
 function App() {
     return (
         <div>
-            <p className="text-red-600">Hello</p>
+            <p className="text-red-600">Hello World</p>
         </div>
     );
 }
