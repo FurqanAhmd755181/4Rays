f26530286ed9d668b75c09f43bbd59984ca7498f
SaadMustafa <saadtanoli445@gmail.com>
2024-10-26 03:49:22 -0700

    resolve the conflict

--- src/LandingPage/Navbar.jsx
+++ src/LandingPage/Navbar.jsx
@@ -2,29 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaChevronDown } from "react-icons/fa";
 import { FiShoppingCart } from "react-icons/fi";
-import { VscAccount } from "react-icons/vsc";
+import { LuUser2 } from "react-icons/lu";
 const Navbar = () => {
   return (
     <div className="px-44">
-      <div className=" px-44 flex items-center justify-center gap-64 border-t-2 border-b-2 border-[#C20009] py-5">
+      <div className=" px-44 flex items-center justify-center gap-64 border-t-2 border-b-2 border-[#D9D9D9] py-5">
         <nav className="flex items-center gap-20">
-          <Link>Features</Link>
-          <Link>Resources</Link>
-          <Link>Prices</Link>
-          <Link>About</Link>
+          <Link className="text-[14px] font-[500] hover:text-[#BC1E2D] hover:underline">
+            Features
+          </Link>
+          <Link className="text-[14px] font-[500] hover:text-[#BC1E2D] hover:underline  ">
+            Resources
+          </Link>
+          <Link className="text-[14px] font-[500] hover:text-[#BC1E2D] hover:underline ">
+            Prices
+          </Link>
+          <Link className="text-[14px] font-[500] hover:text-[#BC1E2D]  hover:underline">
+            About
+          </Link>
         </nav>
         <div className="flex items-center gap-3">
           <div className="flex items-center gap-2">
-            <p>Eng</p>
+            <p className="text-[15px] font-[500]">Eng</p>
             <span>
               <FaChevronDown />
             </span>
           </div>
           <span>
-            <FiShoppingCart size={20} color="red" />
+            <FiShoppingCart size={20} color="#BC1E2D" />
           </span>
           <span>
-            <VscAccount size={20} color="red" />
+            <LuUser2 size={20} color="#BC1E2D" />
           </span>
         </div>
       </div>
