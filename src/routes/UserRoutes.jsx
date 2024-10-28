61b2bf3f99c3e19d198f7050bca1d20dcc773497
Huzaifa Sajjad <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-10-28 08:31:52 -0700

    add blogList

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -3,8 +3,9 @@ import PricingPage from "../pages/User/pricingpage/PricingPage.jsx";
 import AboutPage from "../pages/User/AboutPage/AboutPage.jsx";
 import AccountAuth from "../pages/User/AccountPages/AccountAuth.jsx";
 import MyAccountBody from "../pages/User/AccountPages/MyAccountBody.jsx";
-import SignInPage from "../pages/authpages/Signinpage/Signinpage.jsx";
-import SignUpPage from "../pages/authpages/Signuppage/SignUpPage.jsx";
+
+
+import BlogList from "../pages/User/Blog/BlogList.jsx";
 
 const UserRoutes = [
 
@@ -27,6 +28,10 @@ const UserRoutes = [
     {
         path : "/MyAccount",
         element : <MyAccountBody />
+    },
+    {
+        path : "/bloglist",
+        element : <BlogList />
     }
 ]
 
